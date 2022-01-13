0ab998cdc9c482f17b94ec89474186ded428d3ae
Moira Leon <moira.leon2@gmail.com>
2022-01-12 17:58:27 -0700

    Game State Manager

--- core/src/com/moiraleon/game/FlappyDemo.java
+++ core/src/com/moiraleon/game/FlappyDemo.java
@@ -1,36 +1,46 @@
 package com.moiraleon.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.moiraleon.game.States.GameStateManager;
+import com.moiraleon.game.States.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
 
 	public static final String TITLE = "Flappy Bird";
-
-	SpriteBatch batch;
-	Texture img;
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+//	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		gsm.push(new MenuState(gsm));
+//		img = new Texture("badlogic.jpg");
+		Gdx.gl.glClearColor(1,0,0,1);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		ScreenUtils.clear(1, 0, 0, 1);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
 }
